fc9b30847500977d6fdd8fb6abc1fb9ea6da281a
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2025-09-03 23:54:25 -0300

    Configurações do knex e sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
+import { db } from './database.js'
 
 const app = fastify()
 
 // http://localhost:3333/hello
-app.get('/hello', () => {
-    return 'Hello Nodejs'
+app.get('/hello', async () => {
+    const tables = await db('sqlite_schema').select('*')
+
+    return tables
 })
 
 app.listen({
