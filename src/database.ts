21028bb7154af0b97733e2bd3fb643956ddbd7cd
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2025-09-04 00:59:18 -0300

    Configurações das migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,15 @@
 import knex from 'knex'
 
-export const db = knex({
+export const config = {
     client: 'sqlite3',
     connection: {
-        filename: './tmp/app.db',
+        filename: './db/app.db',
     },
     useNullAsDefault: true,
-})
+    migrations: {
+        extension: "ts",
+        directory: "./db/migrations"
+    },
+}
+
+export const db = knex(config)
