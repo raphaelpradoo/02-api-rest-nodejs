84042915c151ec42fa3e4f0e99a1773f0db48744
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2025-09-13 12:13:21 -0300

    Configurações no server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { db } from './database.js'
 
 const app = fastify()
 
 // http://localhost:3333/hello
 app.get('/hello', async () => {
-    const tables = await db('sqlite_schema').select('*')
+    /*
+    const transaction = await db('transactions').insert({
+        id: crypto.randomUUID(),
+        title: 'Transação teste',
+        amount: 1000,
+    }).returning('*')
 
-    return tables
+    return transaction
+    */
+
+    const transactions = await db('transactions')
+        .select('*')
+        .where('amount', 1000)
+    return transactions
+
 })
 
 app.listen({
